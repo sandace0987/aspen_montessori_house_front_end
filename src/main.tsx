cbef7a065d69df21a14ecb2203552c589f16bcb7
Sandeep Barik <sandy@Sandys-MacBook-Pro-M1.local>
2026-04-10 03:11:57 +0530

    minor changes 2

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootEl = document.getElementById("root")!;
+rootEl.innerHTML = '';  // Clear SEO critical HTML injected at build time
+createRoot(rootEl).render(<App />);
